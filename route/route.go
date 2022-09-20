013c064597f43f6825a9c0a47a491b9912c83801
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-09-20 11:39:18 +0700

    update routes

--- route/route.go
+++ route/route.go
@@ -5,8 +5,19 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+
+	loginhandler "immersiveProject/features/login/delivery"
+	loginrepo "immersiveProject/features/login/repository"
+	loginusecase"immersiveProject/features/login/usecase"
 )
 
 func InitRoutes(e *echo.Echo, db *gorm.DB, cfg *config.AppConfig) {
+	loginRepo := loginrepo.New(db)
+	loginUsecase := loginusecase.New(loginRepo)
+	loginHandler := loginhandler.New(loginUsecase)
 
+	/*  Route  
+				*/
+	
+	e.POST("login", loginHandler.Login)
 }
