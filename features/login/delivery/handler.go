b6dfc9717a0c1b228e519d7975636dfe473ac275
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-09-20 10:29:55 +0700

    add: login

--- features/login/delivery/handler.go
+++ features/login/delivery/handler.go
@@ -1,1 +1,33 @@
 package delivery
+
+import (
+	entity "immersiveProject/features/login/entity"
+	"immersiveProject/utils/helper"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+type loginHandler struct{
+	Usecase entity.UsecaseLogin
+}
+
+func New(usecase entity.UsecaseLogin) *loginHandler {
+	return &loginHandler{
+		Usecase: usecase,
+	}
+}
+
+func (repo *loginHandler) login(c echo.Context)error {
+	loginRequest := loginRequest{}
+	c.Bind(&loginRequest)
+
+	if err := c.Validate(loginRequest); err != nil{
+		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper(err.Error()))
+	}
+	loginEntity := ToEntity(loginRequest)
+	token, err := repo.Usecase.Login(loginEntity)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper(err.Error()))
+	}
+	return c.JSON(http.StatusOK, helper.SuccessDataResponseHelper("Login succses", token))
+}
--- features/login/delivery/request.go
+++ features/login/delivery/request.go
@@ -1,1 +1,16 @@
 package delivery
+
+import (
+	entity "immersiveProject/features/login/entity"
+)
+type loginRequest struct {
+	Email		string	`json:"email"`
+	Password	string	`json:"password"`
+}
+
+func ToEntity(request loginRequest) entity.Login {
+	return entity.Login{
+		Email: request.Email,
+		Password: request.Password,
+	}
+}
