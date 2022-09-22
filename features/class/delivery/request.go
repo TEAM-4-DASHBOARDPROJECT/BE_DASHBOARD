893e0289428b6af43b31121cf27572c97f6d5602
Dian Nurdiana <diannd1507@gmail.com>
2022-09-22 15:24:24 +0700

    fix update class

--- features/class/delivery/request.go
+++ features/class/delivery/request.go
@@ -3,17 +3,17 @@ package delivery
 import "immersiveProject/features/class/entity"
 
 type ClassRequest struct {
-	Name   string `json:"name" form:"name"`
-	Jumlah string `json:"jumlah" form:"jumlah"`
-	Mulai  string `json:"mulai" form:"mulai"`
-	Akhir  string `json:"akhir" form:"akhir"`
+	Name        string `json:"name" form:"name"`
+	JumlahKelas string `json:"jumlah" form:"jumlah"`
+	MulaiKelas  string `json:"mulai" form:"mulai"`
+	AkhirKelas  string `json:"akhir" form:"akhir"`
 }
 
 func RequestToEntity(request ClassRequest) entity.ClassEntity {
 	return entity.ClassEntity{
 		Name:        request.Name,
-		JumlahKelas: request.Jumlah,
-		MulaiKelas:  request.Mulai,
-		AkhirKelas:  request.Akhir,
+		JumlahKelas: request.JumlahKelas,
+		MulaiKelas:  request.MulaiKelas,
+		AkhirKelas:  request.AkhirKelas,
 	}
 }
